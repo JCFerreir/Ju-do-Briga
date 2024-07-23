7db66a88284adff8ad5153c002954cc97efac62a
JCFerreir <juliocesar.ferreira359@outlook.com>
2024-07-23 19:19:13 -0300

    finalizei minha parte

---
+++ js/reveal.js
@@ -0,0 +1,47 @@
+window.revelar = ScrollReveal({reset:true})
+
+//topo
+
+revelar.reveal('.efeito-txt-topo', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-titulo-topo', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'left'
+})
+
+//cards
+
+revelar.reveal('.efeito1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
+
+revelar.reveal('.efeito5', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2500
+})
