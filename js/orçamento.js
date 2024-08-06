dc46f9225d3922b3a213c60fb192464f262cd870
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-08-05 23:01:04 -0300

    implementei um controle de quantidade no form de orçamento

--- js/orçamento.js
+++ js/orçamento.js
@@ -1,21 +1,45 @@
-function sendwhatsapp() {
+function decrementQuantity(inputId) {
+    var input = document.getElementById(inputId);
+    var value = parseInt(input.value);
+    if (value > 0) {
+      input.value = value - 1;
+    }
+  }
+  
+  function incrementQuantity(inputId) {
+    var input = document.getElementById(inputId);
+    var value = parseInt(input.value);
+    input.value = value + 1;
+  }
+  
+  function sendwhatsapp() {
     var numTelefone = "+5511951526585"; // Número de telefone no formato internacional
     var nome = document.getElementById('name').value;
     var email = document.getElementById('email').value;
-
-    // Obter os itens selecionados
+  
+    // Obter os itens selecionados com quantidades
     var items = [];
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    checkboxes.forEach(function(checkbox) {
-        items.push(checkbox.value);
+    var itemQuantities = [
+      {id: 'quantity1', label: 'Item 1'},
+      {id: 'quantity2', label: 'Item 2'},
+      {id: 'quantity3', label: 'Item 3'}
+      // Adicione mais itens conforme necessário
+    ];
+  
+    itemQuantities.forEach(function(item) {
+      var quantity = document.getElementById(item.id).value;
+      if (quantity > 0) {
+        items.push(item.label + " QTD: " + quantity + "x");
+      }
     });
-    
+  
     var itemsText = items.length > 0 ? items.join(", ") : "Nenhum item selecionado";
-
+  
     var url = "https://wa.me/" + numTelefone + "?text="
-        + "*Nome:* " + encodeURIComponent(nome) + "%0a"
-        + "*Email:* " + encodeURIComponent(email) + "%0a"
-        + "*Itens selecionados:* " + encodeURIComponent(itemsText) + "%0a";
-
+      + "*Nome:* " + encodeURIComponent(nome) + "%0a"
+      + "*Email:* " + encodeURIComponent(email) + "%0a"
+      + "*Itens selecionados:* " + encodeURIComponent(itemsText) + "%0a";
+  
     window.open(url, '_blank');
-}
+  }
+  
