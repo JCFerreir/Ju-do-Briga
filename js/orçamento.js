4fb36045b5c447a9ced2a23919d8f81e773a783b
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-09-09 21:24:15 -0300

    implantei as fotos e sistemas novos para o orçamentos

--- js/orçamento.js
+++ js/orçamento.js
@@ -17,23 +17,46 @@ function incrementQuantity(inputId) {
 
 function updateTotal() {
   var total = 0.00;
+  var totalQuantity = 0; // Para rastrear o número total de itens no carrinho
   var itemQuantities = [
-    { id: 'quantity1', price: 2.50 }, // Preço do Item 1
-    { id: 'quantity2', price: 2.50 }, // Preço do Item 2
-    { id: 'quantity3', price: 2.50 }, // Preço do Item 3
-    { id: 'quantity4', price: 200 }, // Preço do Item 4
-    { id: 'quantity5', price: 200 }, // Preço do Item 5
-    { id: 'quantity6', price: 200 } // Preço do Item 6
+    { id: 'quantity1', price: 2.50, discountPrice: 1.20 }, // Crocante
+    { id: 'quantity2', price: 3.00, discountPrice: 1.30 }, // Ferreiro Rcoher
+    { id: 'quantity3', price: 2.50, discountPrice: 1.10 }, // Beijinho
+    { id: 'quantity4', price: 2.50, discountPrice: 1.10 }, // Tradicional
+    { id: 'quantity5', price: 2.50, discountPrice: 1.20 }, // Ninho
+    { id: 'quantity6', price: 2.50, discountPrice: 1.20 }, // Casadinho
+    { id: 'quantity7', price: 3.00, discountPrice: 1.50 }, //M&M
+    { id: 'quantity8', price: 2.50, discountPrice: 1.20 }, //Cajuzinho
+    { id: 'quantity9', price: 2.50, discountPrice: 1.30 }, //Ninho com Uva
+    { id: 'quantity10', price: 2.50, discountPrice: 1.20 }, //Prestigio
+    { id: 'quantity11', price: 2.50, discountPrice: 1.20 }, // Paçoca
+    { id: 'quantity12', price: 10, discountPrice: 10 }, // Pipoca chocolate preto
+    { id: 'quantity13', price: 10, discountPrice: 10 } // Pipoca chocolate branco
     // Adicione mais itens conforme necessário
   ];
 
   itemQuantities.forEach(function(item) {
     var quantity = parseInt(document.getElementById(item.id).value);
-    total += item.price * (isNaN(quantity) ? 0 : quantity);
+    totalQuantity += (isNaN(quantity) ? 0 : quantity); // Soma a quantidade total de itens
   });
+
+  // Verifica se o total de itens é 100 ou mais
+  if (totalQuantity >= 100) {
+    itemQuantities.forEach(function(item) {
+      var quantity = parseInt(document.getElementById(item.id).value);
+      total += item.discountPrice * (isNaN(quantity) ? 0 : quantity); // Aplica o preço com desconto
+    });
+  } else {
+    itemQuantities.forEach(function(item) {
+      var quantity = parseInt(document.getElementById(item.id).value);
+      total += item.price * (isNaN(quantity) ? 0 : quantity); // Preço normal
+    });
+  }
 
   document.getElementById('total-price').textContent = total.toFixed(2);
 }
+
+
 //FINAL DO BOTÃO DE CONTROLE DE QUANTIDADE
 
 
@@ -47,12 +70,19 @@ function sendwhatsapp() {
   // Obter os itens selecionados com quantidades
   var items = [];
   var itemQuantities = [
-    {id: 'quantity1', label: 'Item 1'},
-    {id: 'quantity2', label: 'Item 2'},
-    {id: 'quantity3', label: 'Item 3'},
-    {id: 'quantity4', label: 'Item 4'},
-    {id: 'quantity5', label: 'Item 5'},
-    {id: 'quantity6', label: 'Item 6'}
+    {id: 'quantity1', label: 'Crocante'},
+    {id: 'quantity2', label: 'Ferreiro Rocher'},
+    {id: 'quantity3', label: 'Beijinho'},
+    {id: 'quantity4', label: 'Tradicional'},
+    {id: 'quantity5', label: 'Ninho'},
+    {id: 'quantity6', label: 'Casadinho'},
+    {id: 'quantity7', label: 'M&M'},
+    {id: 'quantity8', label: 'Cajuzinho'},
+    {id: 'quantity9', label: 'Ninho com Uva'},
+    {id: 'quantity10', label: 'Prestigio'},
+    {id: 'quantity11', label: 'Paçoca'},
+    {id: 'quantity12', label: 'Pipoca Gourmet (Chocolate Preto)'},
+    {id: 'quantity13', label: 'Pipoca Gourmet (Chocolate Branco)'}
     // Adicione mais itens conforme necessário
   ];
 
